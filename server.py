b9b3efa10b87f7c8fbde18070b6fbcbb8adbdfd9
Roger Howard <rogerhoward@mac.com>
2017-03-04 21:47:31 -0800

    Add context to routes, and add /info/ endpoint

--- server.py
+++ server.py
@@ -16,13 +16,13 @@ app = flask.Flask(__name__)
 @app.route('/browse/<path:path>')
 def browse(path=None):
     if path:
-        return flask.render_template('zoom.html', path=path)
+        return flask.render_template('zoom.html', path=path, context=config.CONTEXT)
     else:
         s3 = boto3.resource('s3')
         this_bucket = s3.Bucket(config.S3_ZOOM_BUCKET)
         zooms = [x.key for x in this_bucket.objects.all() if x.key.endswith('.dzi')]
 
-        return flask.render_template('home.html', zooms=zooms)
+        return flask.render_template('home.html', zooms=zooms, context=config.CONTEXT)
 
 #------------------------------------------------#
 # System status endpoints                        #
@@ -45,6 +45,19 @@ def list():
 @app.route('/static/<path:filepath>')
 def serve_static(filepath):
     return flask.send_from_directory(config.STATIC_ROOT, filepath)
+
+
+@app.route('/info/')
+def info():
+    """
+    Simple route which lists all the objects in S3_ZOOM_BUCKET
+    Handy for confirming that uploads are working.
+    """
+
+    ENV = dict(os.environ)
+
+    return flask.jsonify({'env': ENV})
+
 
 #------------------------------------------------#
 #  Command line options                          #
